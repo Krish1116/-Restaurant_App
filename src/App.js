baa3ca0dcdf33f9a0e2fc9ab266bfd248a342bf5
Krish1116 <113948600+Krish1116@users.noreply.github.com>
2023-03-28 18:59:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { queryByLabelText } from "@testing-library/react";
+import Header from "./Components/Layout.js/Header";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
